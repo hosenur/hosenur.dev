6cff07708d0c8d5aedf9c63a9990c303aa59d862
HOSENUR <nur08439@gmail.com>
2023-03-09 04:16:06 +0530

    Created Pages

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="flex justify-between max-w-5xl mx-auto p-5">
+      <div className="font-bold text-xl text">hosenur.io</div>
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Space_Mono } from "next/font/google";
+const space = Space_Mono({
+  weight: ["400",'700'],
+  subsets: ["latin"],
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={space.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
