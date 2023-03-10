992105c1b91c06a84d7d9d154c7132fb690963f7
HOSENUR <nur08439@gmail.com>
2023-03-11 04:10:02 +0530

    Fixed Data Fetching

--- src/components/Recent.tsx
+++ src/components/Recent.tsx
@@ -6,9 +6,9 @@ export default function Recent({ data }: { data: any }) {
   console.log(data);
   return (
     <div className="flex space-y-5 flex-col">
-      <Link className="text-4xl" href="https://hosenur.io">
-        CheckOut <span className="gradient">{data.title}</span>{" "}
-      </Link>
+      <span className="text-4xl">
+        CheckOut <Link href="https://hosenur.io" className="gradient">{data.title}</Link>{" "}
+      </span>
       <img src={urlFor(data.mainImage).url()} alt="" />
     </div>
   );
