09302771efbeee33b2ca5c98d339366c09ac38df
HOSENUR <nur08439@gmail.com>
2023-04-01 01:22:52 +0530

    With Rdirect

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,7 +3,7 @@ import { auth } from "../utils/firebase";
 import {
   GithubAuthProvider,
   GoogleAuthProvider,
-  signInWithRedirect,
+  signInWithPopup,
 } from "firebase/auth";
 const AuthContext = React.createContext({});
 export function useAuth() {
@@ -15,11 +15,11 @@ export function AuthProvider({ children }: any) {
   const [loading, setLoading] = useState(false);
   const authGitHub = () => {
     const provider = new GithubAuthProvider();
-    return signInWithRedirect(auth, provider);
+    return signInWithPopup(auth, provider);
   };
   const authGoogle = () => {
     const provider = new GoogleAuthProvider();
-    return signInWithRedirect(auth, provider);
+    return signInWithPopup(auth, provider);
   };
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user: any) => {
