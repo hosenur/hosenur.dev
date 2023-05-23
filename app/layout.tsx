dbd64f1dfbe4c66f8f52f14814a21008311015d2
HOSENUR <nur08439@gmail.com>
2023-05-23 09:17:16 +0530

    Add Footer component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { dankMono, kollektif } from "@/utils/fonts";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -13,12 +14,12 @@ export default function RootLayout({ children }: RootLayoutProps) {
         className={`antialiased min-h-screen bg-white text-slate-900 ${kollektif.className} ${dankMono.variable}`}
       >
         {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem> */}
-          <div className="max-w-2xl mx-auto p-5">
-            <Header/>
-
-            <main className="py-5">{children}</main>
-          </div>
-          {/* <Analytics /> */}
+        <div className="max-w-2xl mx-auto p-5">
+          <Header />
+          <main className="py-5">{children}</main>
+          <Footer />
+        </div>
+        {/* <Analytics /> */}
         {/* </ThemeProvider> */}
       </body>
     </html>
---
+++ components/RecentProjects.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function RecentProjects() {
+  return (
+    <div>RecentProjects</div>
+  )
+}
