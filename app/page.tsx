8189e57413255fba9e75f2451af00c91777f4fd1
HOSENUR <nur08439@gmail.com>
2023-05-23 08:58:28 +0530

    Refactor recent blogs into separate component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import { allBlogs } from "@/.contentlayer/generated";
 import Link from "next/link";
 import { ArrowUpRightIcon } from "@heroicons/react/20/solid";
+import RecentBlogs from "@/components/RecentBlogs";
 export const metadata = {
   title: "Hosenur",
   description: "Personal blog of Hosenur Uzun",
@@ -8,27 +8,8 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <div className="flex flex-col space-y-5">
-      <div className="flex justify-between">
-        <span>RECENT BLOGS</span>
-        <Link
-          href={"/blogs"}
-          className="flex items-center space-x-1 text-slate-500"
-        >
-          <span>VIEW ALL</span>
-          <ArrowUpRightIcon className="w-6 h-6" />
-        </Link>
-      </div>
-      <div className="flex flex-col space-y-2.5">
-        {allBlogs.map((blog) => (
-          <article key={blog._id}>
-            <Link href={blog.slug}>
-              <h2 className="font-bold">{blog.title}</h2>
-            </Link>
-            <p>{blog.description}</p>
-          </article>
-        ))}
-      </div>
+    <div className="flex flex-col gap-5">
+      <RecentBlogs />
     </div>
   );
 }
---
+++ components/RecentBlogs.tsx
@@ -0,0 +1,31 @@
+import { ArrowUpRightIcon } from "@heroicons/react/20/solid";
+import { allBlogs } from "@/.contentlayer/generated";
+import React from "react";
+import Link from "next/link";
+
+export default function RecentBlogs() {
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex justify-between">
+        <span>RECENT BLOGS</span>
+        <Link
+          href={"/blogs"}
+          className="flex items-center space-x-1 text-slate-500"
+        >
+          <span>VIEW ALL</span>
+          <ArrowUpRightIcon className="w-6 h-6" />
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">
+        {allBlogs.map((blog) => (
+          <article key={blog._id}>
+            <Link href={blog.slug}>
+              <h2 className="font-bold">{blog.title}</h2>
+            </Link>
+            <p>{blog.description}</p>
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+}
