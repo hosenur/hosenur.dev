84661cdc791e75c1bc1a79f1d9833337660b8f93
hosenur <nur08439@gmail.com>
2023-09-19 22:25:12 +0530

    Linked Blog Page

--- components/RecentBlogs.tsx
+++ components/RecentBlogs.tsx
@@ -1,5 +1,6 @@
 import { allBlogs, Blog } from '@/.contentlayer/generated'
 import { medium, regular } from '@/utils/fonts'
+import Link from 'next/link'
 import React from 'react'
 
 export default function RecentBlogs() {
@@ -7,10 +8,10 @@ export default function RecentBlogs() {
         <div className='flex flex-col gap-2.5'>
             {allBlogs.map((blog: Blog) => {
                 return (
-                    <div key={blog._id}>
+                    <Link href={blog.slug} key={blog._id}>
                         <h2 className={'text-lg ' + medium.className}>{blog.title}</h2>
                         <p className={'text-zinc-500 ' + regular.className}>{blog.description}</p>
-                    </div>
+                    </Link>
                 )
             }
             )}
