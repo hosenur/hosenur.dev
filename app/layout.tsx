15738442ef57482cdf01686f349929710e4169d2
hosenur <nur08439@gmail.com>
2023-09-19 06:05:40 +0530

    Drafted the hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={"max-w-6xl mx-auto p-10 bg-zinc-900 text-zinc-100"}>{children}</body>
     </html>
   )
 }
---
+++ utils/fonts.tsx
@@ -0,0 +1,4 @@
+import localFont from "next/font/local"
+export const black = localFont({src:"../fonts/black.otf"})
+export const medium = localFont({src:"../fonts/medium.otf"})
+export const regular = localFont({src:"../fonts/regular.otf"})
