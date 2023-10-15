a2cc067c2ae534b5617fedfea263b022bf5d802c
hosenur <nur08439@gmail.com>
2023-10-16 01:16:54 +0530

    Eslint fixes

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -3,7 +3,8 @@ import { RoughNotation } from "react-rough-notation"
 export default function Heading({ text, heading }: any) {
     return (
         <RoughNotation animate={false} show strokeWidth={1.5} type='underline'>
-            {React.createElement(heading, { className: 'inline-block m-0', children: text })}
+            {React.createElement(heading, { className: 'inline-block m-0' }, text )}
+
         </RoughNotation>
     )
 }
