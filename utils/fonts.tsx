d8291ac2e5ed9f39b5495e40d8d8ea3faf2cc5a1
hosenur <nur08439@gmail.com>
2023-10-29 14:29:08 +0530

    Modified Snippet Component

--- utils/fonts.tsx
+++ utils/fonts.tsx
@@ -2,4 +2,4 @@ import localFont from "next/font/local"
 export const black = localFont({ src: "../fonts/black.otf" })
 export const medium = localFont({ src: "../fonts/medium.otf" })
 export const regular = localFont({ src: "../fonts/regular.otf" })
-export const code = localFont({ src: "../fonts/code.woff2" })
+export const code = localFont({ src: "../fonts/code.otf" })
