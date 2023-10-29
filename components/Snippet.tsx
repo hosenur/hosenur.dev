6cdd023f9e758a9c5670ef3d1afccd64b49bffd3
hosenur <nur08439@gmail.com>
2023-10-29 14:28:01 +0530

    Created Snippet Component

--- components/Snippet.tsx
+++ components/Snippet.tsx
@@ -1,32 +1,36 @@
 'use client';
 import React from 'react'
-import { Highlight, themes } from "prism-react-renderer"
-import { SiTypescript } from "@icons-pack/react-simple-icons"
+import { Highlight, themes, Prism } from "prism-react-renderer"
+(typeof global !== "undefined" ? global : window).Prism = Prism
+require("prismjs/components/prism-bash")
+require("prismjs/components/prism-lua")
+import { SiTypescript, SiGnubash, SiLua } from "@icons-pack/react-simple-icons"
 import { CopyIcon } from 'lucide-react';
 import { code } from '@/utils/fonts';
-export default function Snippet(props: {}) {
-  const codeBlock = `const GroceryItem: React.FC<GroceryItemProps> = ({ item }) => {
-  return (
-    <div>
-      <h2>{item.name}</h2>
-      <p>Price: {item.price}</p>
-      <p>Quantity: {item.quantity}</p>
-    </div>
-  );
-}`
+const iconMappings: any
+  = {
+  "tsx": <SiTypescript size={15} />,
+  "bash": <SiGnubash size={15} />,
+  "lua" : <SiLua size={15} />
+}
+
+export default function Snippet({ language, fileName, codeBlock }: any) {
   return (
     <div>
       <Highlight
-        theme={themes.shadesOfPurple}
-        code={codeBlock}
-        language="tsx"
+        theme={themes.nightOwl}
+        code={codeBlock.trim()}
+        language={language}
       >
         {({ style, tokens, getLineProps, getTokenProps }) => (
           <pre className='border border-zinc-800 p-0 bg-zinc-900'>
             <div className='flex mx-0 gap-2.5 items-center  px-5 py-2.5  border-b border-zinc-800 bg-zinc-950 mb-2.5 text-zinc-400'>
               <>
-                <SiTypescript size={15} />
-                <span>pages/index.tsx</span>
+                {
+                  iconMappings[language]}
+                <span>{
+                  fileName ? fileName : language.charAt(0).toUpperCase() + language.slice(1)
+                }</span>
               </>
               <CopyIcon size={20} className='ml-auto cursor-pointer' />
 
