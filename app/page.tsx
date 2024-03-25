82e1acd7632d1d98e1f9edddb279de088bb3b6b1
hosenur <nur08439@gmail.com>
2024-03-26 03:45:03 +0530

    Commented Out Code

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className='flex flex-col p-5 max-w-4xl mx-auto'>
       <main className='flex flex-col gap-10'>
-        <Hero/>
+        {/* <Hero/> */}
         <RecentBlogs/>  
       </main>
     </div>
