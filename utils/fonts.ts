10fe39860da2e3680e024a11177b887bc7754c3d
hosenur <nur08439@gmail.com>
2024-04-01 09:52:26 +0530

    fix: fix font names

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,6 +1,5 @@
 
 import localFont from "next/font/local"
-import {Space_Mono} from "next/font/google"
 export const code = localFont({ src: "../fonts/code.woff2" })
 export const regular = localFont({ src: "../fonts/regular.ttf" })
 export const medium = localFont({ src: "../fonts/medium.ttf" })
