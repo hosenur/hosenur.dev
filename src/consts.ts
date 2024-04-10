344bd0b7bb96abda78a227f5181f36537af54677
hosenur <mail@hosenur.dev>
2024-04-10 20:59:21 +0530

    fix meta

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'hosenur.dev';
+export const SITE_DESCRIPTION = 'Indie Developer, Full Stack Web & App Dev, Linux Enthusiast and CS Student, Building products solo.';
