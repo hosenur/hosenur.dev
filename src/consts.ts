f45efa1bd1e8cc143d1d7b34a8f53509e12fa0b4
hosenur <hosenur.dev@gmail.com>
2024-05-12 20:59:40 +0530

    refactor: migrate to astro

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'hosenur.dev';
-export const SITE_DESCRIPTION = 'Indie Developer, Full Stack Web & App Dev, Linux Enthusiast and CS Student, Building products solo.';
