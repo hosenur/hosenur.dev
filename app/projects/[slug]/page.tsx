3fd8e3330655a47bf6a7c5a3f67ceec0556a5fb1
hosenur <hosenur.dev@gmail.com>
2024-05-16 19:23:47 +0530

    feat: create project page

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,17 @@
+import { allProjects } from '@/data'
+
+export function generateStaticParams() {
+    return allProjects.paths().map((pathname) => ({ slug: pathname }))
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+    const { Content, frontMatter } = (await allProjects.get(params.slug))!
+    if (!Content) return null
+    return (
+        <>
+            <div className='bg-white w-full min-h-screen  p-5 rounded-lg shadow-lg border border-zinc-100'>
+                <Content />
+            </div>
+        </>
+    )
+}
