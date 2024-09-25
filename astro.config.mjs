95fbb8d585af42aea9c8d7f4ce5c20b234c42c79
hosenur <hosenur.dev@gmail.com>
2024-09-25 13:36:28 +0530

    removed vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
 
 
 import sitemap from '@astrojs/sitemap';
@@ -11,10 +10,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
     site: 'https://hosenur.dev',
     integrations: [mdx(), sitemap(), tailwind()],
-    adapter: vercel({
-        webAnalytics: { enabled: true }
-    })
 });
